80da8d7f22def97d09f12c312cf0ac9e9423db47
Mitch Birti <yahooguntu@gmail.com>
2015-11-01 11:38:54 -0600

    Added WAV support.

--- server.rb
+++ server.rb
@@ -21,11 +21,22 @@ end
 post '/upload', provides: :json do
   begin
     filename = params['file'][:filename]
-    FileUtils.mv(params['file'][:tempfile].path, "./sounds/#{filename}")
-    ObjectSpace.undefine_finalizer(params['file'][:tempfile])
-    {status: :ok, filename: filename}
-  rescue
-    {status: :not_ok}
+    if filename.downcase.end_with? ".mp3"
+      FileUtils.mv(params['file'][:tempfile].path, "./sounds/#{filename}")
+      ObjectSpace.undefine_finalizer(params['file'][:tempfile])
+      {status: :ok, filename: filename}
+    elsif filename.downcase.end_with? ".wav"
+      temp_path = params['file'][:tempfile].path.gsub(/\ /, '\ ')
+      new_filename = filename.gsub(/\ /, '\ ').gsub(/.wav$/i, ".mp3")
+      require 'byebug'
+      byebug
+      Open3.popen3("ffmpeg -i #{temp_path} -vn -ar 44100 -ac 2 -ab 192k -f mp3 ./sounds/#{new_filename}")
+      {status: :ok, filename: filename}
+    else
+      {status: :failure, error: "Unsupported file type."}
+    end
+  rescue StandardError => e
+    {status: :not_ok, error: e.inspect}
   end
 end
 
